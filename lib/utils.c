399700e32b12b1ec7785cdca2e7e593394f13a07
alon118 <alonik09@gmail.com>
2026-06-03 18:07:09 +0300

    Update utils.c

--- lib/utils.c
+++ lib/utils.c
@@ -8,8 +8,8 @@ int gettype(char *s, char *t){
             *t = rCommands[i].name;
             return 1;
         }
-        if(i <= 4 && strcmp(*s, rCommands[i].name)){
-            *t = rCommands[i].name;
+        if(i <= 4 && strcmp(*s, iCommands[i].name)){
+            *t = iCommands[i].name;
             return 1;
         }
         if(i <= 16 && strcmp(*s, jCommands[i].name)){
