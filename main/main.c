3770e1b747e0f066368c45b71659cd0520be41a4
noamman27 <153654176+noamman27@users.noreply.github.com>
2026-06-03 18:03:48 +0300

    c

--- main/main.c
+++ main/main.c
@@ -4,14 +4,14 @@
 
 int main(int argc, char *argv[]){
     int i;
-    FILE *f, *write;
+    FILE *original, *pre_assembled;
     if(argc < 2){ /*make sure we were given at least one file*/
         fprintf(stderr, "not enough given params");
         return 1;
     }
     for(i=1; i<argc; i++){
-        f=fopen(argv[i], "r"), write = fopen(strcat(argv[i],"-PP"),"w");
-        if(!pre_assemble(f, write)){ /*call pre assembler and make suer no errors were given*/
+        original=fopen(argv[i], "r"), pre_assembled = fopen(strcat(argv[i],"-PP"),"w");
+        if(!pre_assemble(original, pre_assembled)){ /*call pre assembler and make suer no errors were given*/
             return 1; /* if errors were given we just return 1 since pre_assemble prints errors*/
         }
         /*TODO: once we implement next passes call them*/
